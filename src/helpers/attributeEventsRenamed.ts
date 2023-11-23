de4648e1f9005480c22575ea1d857ab54cd30923
Conky_刘 <949687387@qq.com>
2023-11-23 01:41:31 -0600

    Update attributeEventsRenamed.ts
    use angular-eslint

--- src/helpers/attributeEventsRenamed.ts
+++ src/helpers/attributeEventsRenamed.ts
@@ -1,14 +1,16 @@
-import * as ast from '@angular/compiler';
-import * as Lint from 'tslint';
-import { BasicTemplateAstVisitor } from 'codelyzer/angular/templates/basicTemplateAstVisitor';
+import { Rule } from '@angular-eslint/template-parser';
+import { BasicTemplateAstVisitor } from '@angular-eslint/template-parser/builder';
 
 function generateErrorMessage(elementName: string, attrName: string, replacement: string) {
   return `The ${attrName} event of ${elementName} has been renamed. Use ${replacement} instead.`;
 }
 
-export function createAttributesRenamedTemplateVisitorClass(elementNames: string[] | undefined, replacementMap: Map<string, string>) {
+export function createAttributesRenamedTemplateVisitorClass(
+  elementNames: string[] | undefined,
+  replacementMap: Map<string, string>
+) {
   return class extends BasicTemplateAstVisitor {
-    visitElement(element: ast.ElementAst, context: any): any {
+    visitElement(element: Rule.ElementAst, context: any): any {
       if (!elementNames || elementNames.includes(element.name)) {
         this.checkAttributesForReplacements(element);
       }
@@ -16,7 +18,7 @@ export function createAttributesRenamedTemplateVisitorClass(elementNames: string
       super.visitElement(element, context);
     }
 
-    private checkAttributesForReplacements(element: ast.ElementAst) {
+    private checkAttributesForReplacements(element: Rule.ElementAst) {
       for (const output of element.outputs) {
         const replacement = replacementMap.get(output.name);
 
@@ -26,7 +28,7 @@ export function createAttributesRenamedTemplateVisitorClass(elementNames: string
           const position = this.getSourcePosition(start);
 
           this.addFailureAt(start, length, generateErrorMessage(element.name, output.name, replacement), [
-            Lint.Replacement.replaceFromTo(position, position + length, replacement)
+            this.createReplacement(position, length, replacement),
           ]);
         }
       }
