7afac4bb94c68f7fdc59bad876966bc0ff46fc73
Conky_刘 <949687387@qq.com>
2023-11-23 01:50:31 -0600

    Update ionLabelRequired.ts
    eslint

--- src/helpers/ionLabelRequired.ts
+++ src/helpers/ionLabelRequired.ts
@@ -1,20 +1,27 @@
-import * as ast from '@angular/compiler';
-import { BasicTemplateAstVisitor } from 'codelyzer/angular/templates/basicTemplateAstVisitor';
+import { Rule, ASTWithSource, ParseSourceSpan } from '@angular-eslint/template-parser';
+import { BasicTemplateAstVisitor } from '@angular-eslint/template-parser/builder';
+import * as ts from 'typescript';
 
 import { isElementAst } from './utils';
 
 export function createIonLabelRequiredTemplateVisitorClass(elementName: string) {
   return class extends BasicTemplateAstVisitor {
-    visitElement(element: ast.ElementAst, context: any): any {
+    visitElement(element: Rule.ElementAst, context: any): any {
       if (element.name && element.name === elementName) {
-        const ionLabelElement = element.children.find((e): e is ast.ElementAst => isElementAst(e) && e.name === 'ion-label');
+        const ionLabelElement = element.children.find((e): e is Rule.ElementAst =>
+          isElementAst(e) && e.name === 'ion-label'
+        );
 
         if (!ionLabelElement) {
           const start = element.sourceSpan.start.offset;
           const length = element.name.length;
           const position = this.getSourcePosition(start) + length + 1;
 
-          this.addFailureAt(start + 1, length, `The ${elementName} requires an ion-label component. It is no longer automatically added.`);
+          this.addFailureAt(
+            start + 1,
+            length,
+            `The ${elementName} requires an ion-label component. It is no longer automatically added.`
+          );
         }
       }
 
