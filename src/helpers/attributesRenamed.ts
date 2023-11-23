96ca76837022d64b7555a3a4f96e00c900b1d83e
Conky_刘 <949687387@qq.com>
2023-11-23 01:45:37 -0600

    Update attributesRenamed.ts
    eslint

--- src/helpers/attributesRenamed.ts
+++ src/helpers/attributesRenamed.ts
@@ -1,6 +1,5 @@
-import * as ast from '@angular/compiler';
-import * as Lint from 'tslint';
-import { BasicTemplateAstVisitor } from 'codelyzer/angular/templates/basicTemplateAstVisitor';
+import { Rule, ASTWithSource, ParseSourceSpan } from '@angular-eslint/template-parser';
+import { BasicTemplateAstVisitor } from '@angular-eslint/template-parser/builder';
 
 function generateErrorMessage(elementName: string, attrName: string, replacement: string) {
   return `The ${attrName} attribute of ${elementName} has been renamed. Use ${replacement} instead.`;
@@ -8,7 +7,7 @@ function generateErrorMessage(elementName: string, attrName: string, replacement
 
 export function createAttributesRenamedTemplateVisitorClass(elementNames: string[] | undefined, replacementMap: Map<string, string>) {
   return class extends BasicTemplateAstVisitor {
-    visitElement(element: ast.ElementAst, context: any): any {
+    visitElement(element: Rule.ElementAst, context: any): any {
       if (!elementNames || elementNames.includes(element.name)) {
         this.checkAttributesForReplacements(element);
       }
@@ -16,7 +15,7 @@ export function createAttributesRenamedTemplateVisitorClass(elementNames: string
       super.visitElement(element, context);
     }
 
-    private checkAttributesForReplacements(element: ast.ElementAst) {
+    private checkAttributesForReplacements(element: Rule.ElementAst) {
       for (const attr of element.attrs) {
         const replacement = replacementMap.get(attr.name);
 
@@ -26,10 +25,14 @@ export function createAttributesRenamedTemplateVisitorClass(elementNames: string
           const position = this.getSourcePosition(start);
 
           this.addFailureAt(start, length, generateErrorMessage(element.name, attr.name, replacement), [
-            Lint.Replacement.replaceFromTo(position, position + length, replacement)
+            this.createReplacement(position, length, replacement),
           ]);
         }
       }
+    }
+
+    private createReplacement(position: ParseSourceSpan, length: number, text: string) {
+      return new Lint.Replacement(position.start.offset, length, text);
     }
   };
 }
